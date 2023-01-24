9b7240f98f5f75e3911d937ee1de3b8e60eaa1d5
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-01-24 20:33:26 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+
+####
+    #    1. Remove the 
+    #    <Invoice><ext:UBLExtensions/>
+    #     block 
+    #    2. Remove the 
+    #    <invoice><cac:Additional
+    #    DocumentReference/> 
+    #    block where <cbc:ID/> = 
+    #    QR 
+    #    3. Remove the 
+    #    <invoice><cac:Signature/> 
+    #    block  
+    #    4. Canonicalize the Invoice 
+    #    using the C14N11 
+    #    standard 
+    #   5. Hash the resulting string 
+    #   using SHA256 to a binary 
+    #   object 
+    #   6. Base64 encode the binary 
+    #   object to generate the 
+    #   digest value 
+    #
+    #
+    ####
+
+from xml.dom.minidom import parse, Node
+
+##############################################################
+def xmlRemoveElement(elementName:str):
+    nodes = document.getElementsByTagName(elementName)
+    for node in nodes:
+        parent = node.parentNode
+        parent.removeChild(node) 
+###############################################################
+document = parse('invoice.xml')
+
+
+xmlRemoveElement("ext:UBLExtensions")
+
+##  Next Step find the Element cac:AdditionalDocumentReference and has Child cbc:ID with text QR
+##  <cac:AdditionalDocumentReference>
+##       <cbc:ID>QR</cbc:ID>
+##
+##
+
+nodes = document.getElementsByTagName("cac:AdditionalDocumentReference")
+
+for node in nodes:
+     nodeX = node.getElementsByTagName("cbc:ID")
+     if nodeX[0].firstChild.nodeValue =="QR":
+        parent = node.parentNode
+        parent.removeChild(node) 
+        break
+
+
+## next step remove cac:Signature
+xmlRemoveElement("cac:Signature")
+
+print(document.toxml())
+
+
+#with open("result.xml","w") as fs:
+#    fs.write(document.toxml())
+#    fs.close()
